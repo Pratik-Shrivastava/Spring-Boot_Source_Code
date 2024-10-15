861bb45f60a3ce05b6aed36418f83cde16f0b702
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-10-15 11:47:40 +0530

    feat: add new student entry feature

--- src/JDBC/Main.java
+++ src/JDBC/Main.java
@@ -1,5 +1,6 @@
 package JDBC;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +11,7 @@ public class Main {
 
         while(true) {
             System.out.println("1. Fetch student using id.");
-            System.out.println("2. Fetch student list.");
+            System.out.println("2. Fetch all students.");
             System.out.println("3. Insert an entry in student table.");
             System.out.println("4. Delete an entry in student table.");
             System.out.print("Enter your choice: ");
@@ -18,6 +19,7 @@ public class Main {
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
+
                     System.out.print("Enter the id: ");
                     int id = sc.nextInt();
                     Student student = crudOperation.getStudentById(id);
@@ -30,7 +32,40 @@ public class Main {
                     break;
 
                 case 2:
+
+                    List<Student> studentList = crudOperation.getStudentList();
+
+                    if(studentList == null || studentList.isEmpty()) {
+                        System.out.println("Student table is empty!");
+                    } else {
+                        System.out.println(studentList);
+                    }
+
+                    break;
+
                 case 3:
+
+                    Student newStudent = new Student();
+
+                    System.out.print("Enter the name of the student: ");
+                    newStudent.setName(sc.next());
+
+                    System.out.print("Enter the age of the student: ");
+                    newStudent.setAge(sc.nextInt());
+
+                    System.out.print("Enter the marks of the student: ");
+                    newStudent.setMarks(sc.nextDouble());
+
+                    boolean added = crudOperation.addNewStudent(newStudent);
+
+                    if(added) {
+                        System.out.println("New student added successfully!");
+                    } else {
+                        System.out.println("Failed to add new student!");
+                    }
+
+                    break;
+
                 case 4:
                     break;
 
