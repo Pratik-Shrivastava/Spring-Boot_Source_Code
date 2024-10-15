a72fc9f744ca26577217ad65f6ba037bf3b222cf
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-10-15 11:25:33 +0530

    feat: add getStudentList() method

--- src/JDBC/CrudOperation.java
+++ src/JDBC/CrudOperation.java
@@ -1,9 +1,8 @@
 package JDBC;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CrudOperation {
 
@@ -35,10 +34,43 @@ public class CrudOperation {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
         return null;
-
     }
+
+
+    public List<Student> getStudentList() {
+        Connection connection = JdbcConfig.getConnectionObject();
+        List<Student> studentList = new ArrayList<>();
+
+        if(connection == null) {
+            System.out.println("Failed to establish connection with database!");
+            return studentList;
+        }
+
+        String query = "SELECT * FROM student;";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+
+            while(rs.next()) {
+                studentList.add(
+                        new Student(
+                                rs.getInt(1),
+                                rs.getString(2),
+                                rs.getInt(3),
+                                rs.getDouble(4)
+                        )
+                );
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return studentList;
+    }
+
+
 
 
 }
